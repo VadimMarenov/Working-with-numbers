d01c5ffe7636e7acaa769848c2eb3d9f24da100d
Vadim <v.marenov@bk.ru>
2022-03-23 15:38:02 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> result = new ArrayList<>();
+        for (int num : list) {
+            if (num > 0 && num % 2 == 0) result.add(num);
+        }
+        Collections.sort(result);
+        for (int num : result) System.out.println(num);
+    }
+
+}
